671690ecc082ebe2a38623293f1f367180b18d44
Charlie Jiang <cqjjjzr@126.com>
2020-08-13 15:10:16 +0800

    :fire: (NW): Remove kotlin

---
+++ src/main/java/dd/center/vnerve/data/bilibili/live/PopularityUpdateMessage.java
@@ -0,0 +1,8 @@
+package dd.center.vnerve.data.bilibili.live;
+
+/**
+ * @author Charlie Jiang
+ * @since rv1
+ */
+public class PopularityUpdateMessage {
+}
---
+++ src/main/java/dd/center/vnerve/neuronwire/deserialize/ProtobufDeserializer.java
@@ -0,0 +1,8 @@
+package dd.center.vnerve.neuronwire.deserialize;
+
+/**
+ * @author Charlie Jiang
+ * @since rv1
+ */
+public class ProtobufDeserializer {
+}
